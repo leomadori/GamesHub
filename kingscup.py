c604accd49360e0b9cb03a2b2d54ed9d1dc3e3a3
NigelTCK <nigeltan22@gmail.com>
2024-12-11 14:01:07 +0100

    Update kingscup.py

--- kingscup.py
+++ kingscup.py
@@ -1,48 +1,46 @@
 import os
-import random
+import random #import random to randomise the deck of cards in build_deck
 import streamlit as st
 
 # Function to build and shuffle the deck
-def build_deck(include_jokers=False):
-    suits = {'Hearts': 'H', 'Diamonds': 'D', 'Clubs': 'C', 'Spades': 'S'}
-    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-    deck = [f"{value} of {suit}" for suit in suits for value in values]
-    if include_jokers:
-        deck.extend(['Joker', 'Joker'])
-    random.shuffle(deck)
-    return deck
+def build_deck(): #A function that builds a classic deck of 52 cards
+    suits = {'Hearts': 'H', 'Diamonds': 'D', 'Clubs': 'C', 'Spades': 'S'} #A dictionary that references the 4 card suits in a classic deck of cards
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] #A dictionary that references the values of the cards in a classic deck of cards
+    deck = [f"{value} of {suit}" for suit in suits for value in values] #A list that adds 52 cards - 13 different values over 4 different suits
+    random.shuffle(deck) #randomises the deck
+    return deck #returns a shuffled deck of 52 cards (or 54 if include_jokers=True)
 
 # Card Image Path Helper
 def get_card_image_path(card):
     """Convert full card name to the corresponding image path."""
-    suits_abbreviation = {'Hearts': 'H', 'Diamonds': 'D', 'Clubs': 'C', 'Spades': 'S'}
-    value, suit = card.split(" of ")
+    suits_abbreviation = {'Hearts': 'H', 'Diamonds': 'D', 'Clubs': 'C', 'Spades': 'S'} #Dictionary that calls the suit of the card's image
+    value, suit = card.split(" of ") #e.g. 3 of Spades
     card_filename = value + suits_abbreviation[suit] + ".jpg"  # e.g., '3S.jpg'
     return os.path.join("cards", card_filename)
 
 # Initialize Streamlit Session State
 if 'deck' not in st.session_state:
-    st.session_state.deck = build_deck()
+    st.session_state.deck = build_deck() 
 if 'kings_drawn' not in st.session_state:
     st.session_state.kings_drawn = 0
 if 'last_card' not in st.session_state:
     st.session_state.last_card = None
 
 # Actions for cards
-actions = {
-    '2': "Pick someone to drink!",
-    '3': "You drink!",
-    '4': "All GUYS drink!",
-    '5': "Play Five Ten!",
-    '6': "All LADIES drink!",
-    '7': "Last person to stand up drinks!",
-    '8': "Pick someone to drink with you!",
-    '9': "Say a word, next person must rhyme. Loser drinks!",
-    '10': "Pick a category. Go around until someone can't answer. They drink!",
-    'J': "Never Have I Ever - Everyone puts up 3 fingers. First to drop all fingers drinks!",
-    'Q': "Silent Queen - No one can talk to you until the next Queen. Rule-breakers drink!",
-    'K': "King's Cup! Pour a bit of your drink into the cup. The 4th King drinks it all!",
-    'A': "Set a new rule that lasts the game! Be creative!"
+actions = { #A dictionary that calls an action for the corresponding value of the card
+    '2': "Pick someone to drink!", #The action when the card's value is 2
+    '3': "You drink!", #The action when the card's value is 3
+    '4': "All GUYS drink!", #The action when the card's value is 4
+    '5': "Play Five Ten!", #The action when the card's value is 5
+    '6': "All LADIES drink!", #The action when the card's value is 6
+    '7': "Last person to stand up drinks!", #The action when the card's value is 7
+    '8': "Pick someone to drink with you!", #The action when the card's value is 8
+    '9': "Say a word, next person must rhyme. Loser drinks!", #The action when the card's value is 9
+    '10': "Pick a category. Go around until someone can't answer. They drink!", #The action when the card's value is 10
+    'J': "Never Have I Ever - Everyone puts up 3 fingers. First to drop all fingers drinks!", #The action when the card's value is J
+    'Q': "Silent Queen - No one can talk to you until the next Queen. Rule-breakers drink!", #The action when the card's value is Q
+    'K': "King's Cup! Pour a bit of your drink into the cup. The 4th King drinks it all!", #The action when the card's value is K
+    'A': "Set a new rule that lasts the game! Be creative!" #The action when the card's value is A
 }
 
 # Streamlit UI
