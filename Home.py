83f28d2c5c899c89b0ef2f52f3c27e6fafdc6858
leomadori <leonardoreina153@gmail.com>
2024-12-19 14:38:25 +0100

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,26 @@
+import streamlit as st
+st.header("Welcome to GamesHub")
+
+st.image("Images/GH_logo.webp")
+
+st.write("GamesHub is a Webapp that integrates various card games for the sake of having fun!")
+st.write("Please use the sidebar menu to navigate between our games")
+st.divider()
+
+
+st.subheader("Party Games!:partying_face:")
+st.write("Drink responsibly with our KingsCup game! Simply the perfect game for a party")
+st.divider()
+
+st.subheader("Test your Knowledge!:brain:")
+st.write("Head over to the Trivia Master to put your knowledge to the test in a series of general knowledge trivia!")
+st.divider()
+
+st.subheader("Feeling Lucky?:four_leaf_clover:")
+st.write("If you feel like this is you lucky day, head over to the BlackJack section and give it a try!")
+st.divider() 
+
+
+
+st.write("GamesHub is an Open source project created by students of the Univesrity of St.Gallen")
+st.write("Feel free to download the code and add you own game thus contributing to the platform!")
